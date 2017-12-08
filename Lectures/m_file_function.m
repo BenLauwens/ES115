fa3c0a55182153497ddc285cc83e7e1520ff1e56
Ben Lauwens <ben.lauwens@gmail.com>
2017-12-08 11:28:08 +0100

    Add Lecture 9

---
+++ Lectures/m_file_function.m
@@ -0,0 +1,7 @@
+function out = m_file_function(in)
+    % m_file_function: Example function stored in a M-file.
+    % in: numerical argument
+    % out: numerical argument
+    % out = 2 * in
+    out = 2 * in;
+end
